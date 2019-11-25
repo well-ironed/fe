1f26d6b32385b2aff3fb181202c982f194aed169
Rafał Studnicki <studzien@gmail.com>
2019-11-25 14:08:08 +0100

    Bump to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FE.MixProject do
   def project do
     [
       app: :fe,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
