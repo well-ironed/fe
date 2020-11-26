4baf290a26cf45a192bb06080192fc6914d2293b
Jacobo Bouzas <nireiorange@gmx.com>
2020-11-26 22:23:16 +0100

    Add a curry function

--- lib/fe.ex
+++ lib/fe.ex
@@ -15,4 +15,21 @@ defmodule FE do
   """
   @spec const(a) :: (any -> a) when a: var
   def const(a), do: fn _ -> a end
+  
+  @doc """
+  Transforms a n-ary function in an unary one that accepts its first
+  argument and returns a function expecting the next one, successively
+  """
+  @spec curry((a, ... -> any())) :: (a -> any()) when a: var
+  def curry(fun) do
+    {_, arity} = :erlang.fun_info(fun, :arity)
+    curry(fun, arity, [])
+  end
+
+  defp curry(fun, 0, args) do
+    apply(fun, Enum.reverse args)
+  end
+  defp curry(fun, arity, args) do
+    fn arg -> curry(fun, arity - 1, [arg | args]) end
+  end
 end
