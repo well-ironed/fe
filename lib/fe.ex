f622b7886331fe55ccac738ea2345f8eff74a1e9
Kamil Zieliński <paryzowy@gmail.com>
2021-02-26 18:44:18 +0100

    add curry with tests

--- lib/fe.ex
+++ lib/fe.ex
@@ -15,4 +15,24 @@ defmodule FE do
   """
   @spec const(a) :: (any -> a) when a: var
   def const(a), do: fn _ -> a end
+
+  @doc """
+  Transforms a n-ary function in an unary one that accepts its first
+  argument and returns a function expecting the next one, successively
+  """
+  @spec curry((... -> any())) :: (any() -> any())
+  def curry(fun) do
+    case :erlang.fun_info(fun, :arity) do
+      {_, 0} -> raise ArgumentError, "0-ary functions cannot be curried"
+      {_, arity} -> curry(fun, arity, [])
+    end
+  end
+
+  defp curry(fun, 0, args) do
+    apply(fun, Enum.reverse(args))
+  end
+
+  defp curry(fun, arity, args) do
+    fn arg -> curry(fun, arity - 1, [arg | args]) end
+  end
 end
--- test/fe_test.exs
+++ test/fe_test.exs
@@ -20,4 +20,29 @@ defmodule FETest do
     assert FE.const(a_number).(a_tuple) == a_number
     assert FE.const(a_tuple).(some_binary) == a_tuple
   end
+
+  describe "curry/1" do
+    test "function with arity bigger than 0 is curried" do
+      fun = fn x -> x end
+      result = FE.curry(fun).(1)
+      assert result == 1
+
+      fun = fn x, y -> x + y end
+
+      add5 = FE.curry(fun).(5)
+      assert add5.(3) == 8
+
+      result = FE.curry(fun).(1).(2)
+      assert result == 3
+
+      fun = fn x, y, z -> x + y + z end
+      result = FE.curry(fun).(1).(2).(3)
+      assert result == 6
+    end
+
+    test "function with arity equal 0 raises error" do
+      fun = fn -> :ok end
+      assert_raise(ArgumentError, fn -> FE.curry(fun) end)
+    end
+  end
 end
